77d79f303c85bc90ec12b68e2cee6b08ff4fd013
Kuba Niżyński <kubanizynski@wp.pl>
2025-05-27 20:29:57 +0200

    feat: added create shipment

---
+++ internal/domain/shipment/constants.go
@@ -0,0 +1,3 @@
+package shipment
+
+var DefaultPackSizes = []int{250, 500, 1000, 2000, 5000}
---
+++ internal/domain/shipment/shipment.go
@@ -0,0 +1,55 @@
+package shipment
+
+import (
+	"math"
+	"sort"
+)
+
+type Shipment struct {
+	PacksUsed map[int]int
+}
+
+func (s *Shipment) Create(itemsOrdered int, packSizes []int) {
+	sort.Sort(sort.Reverse(sort.IntSlice(packSizes)))
+
+	best := make(map[int]int)
+	minExtra := math.MaxInt
+	minPacks := math.MaxInt
+
+	var dfs func(idx, total, packs int, used map[int]int)
+	dfs = func(idx, total, packs int, used map[int]int) {
+		if total >= itemsOrdered {
+			extra := total - itemsOrdered
+			if extra < minExtra || (extra == minExtra && packs < minPacks) {
+				minExtra = extra
+				minPacks = packs
+				best = make(map[int]int)
+				for k, v := range used {
+					if v > 0 {
+						best[k] = v
+					}
+				}
+			}
+			return
+		}
+		if idx >= len(packSizes) {
+			return
+		}
+
+		size := packSizes[idx]
+		maxCount := (itemsOrdered + size - 1) / size
+		for i := 0; i <= maxCount; i++ {
+			next := make(map[int]int)
+			for k, v := range used {
+				next[k] = v
+			}
+			if i > 0 {
+				next[size] = i
+			}
+			dfs(idx+1, total+i*size, packs+i, next)
+		}
+	}
+
+	dfs(0, 0, 0, make(map[int]int))
+	s.PacksUsed = best
+}
