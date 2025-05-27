918e8be324d60b949922fe5117c744f279b67fc6
Kuba Niżyński <kubanizynski@wp.pl>
2025-05-27 20:31:05 +0200

    test: improved packing tests

--- internal/domain/packing/packing_test.go
+++ internal/domain/packing/packing_test.go
@@ -11,49 +11,49 @@ var testPackSizes = []int{250, 500, 1000, 2000, 5000}
 func TestPackingResult_Calculate(t *testing.T) {
 	tests := []struct {
 		name     string
-		amount   int
+		items    int
 		expected map[int]int
 		err      error
 	}{
 		{
-			name:     "1 item",
-			amount:   1,
+			name:     "When 1 item ordered",
+			items:    1,
 			expected: map[int]int{250: 1},
 			err:      nil,
 		},
 		{
-			name:     "Exact 250",
-			amount:   250,
+			name:     "When 250 items ordered - matches exactly one of the existing packages",
+			items:    250,
 			expected: map[int]int{250: 1},
 			err:      nil,
 		},
 		{
-			name:     "251 items",
-			amount:   251,
+			name:     "When 251 items ordered",
+			items:    251,
 			expected: map[int]int{500: 1},
 			err:      nil,
 		},
 		{
-			name:     "501 items",
-			amount:   501,
+			name:     "When 501 items ordered",
+			items:    501,
 			expected: map[int]int{250: 1, 500: 1},
 			err:      nil,
 		},
 		{
-			name:     "12001 items",
-			amount:   12001,
+			name:     "When 12001 items ordered",
+			items:    12001,
 			expected: map[int]int{250: 1, 2000: 1, 5000: 2},
 			err:      nil,
 		},
 		{
-			name:     "Invalid zero items",
-			amount:   0,
+			name:     "Wrong - Invalid zero items",
+			items:    0,
 			expected: nil,
 			err:      ErrInvalidItemCount,
 		},
 		{
-			name:     "Negative items",
-			amount:   -10,
+			name:     "Wrong - Negative items",
+			items:    -10,
 			expected: nil,
 			err:      ErrInvalidItemCount,
 		},
@@ -62,7 +62,7 @@ func TestPackingResult_Calculate(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			p := &PackingResult{}
-			err := p.Calculate(tt.amount, testPackSizes)
+			err := p.Calculate(tt.items, testPackSizes)
 			assert.ErrorIs(t, err, tt.err)
 			if tt.err == nil {
 				assert.Equal(t, tt.expected, p.PacksUsed)
