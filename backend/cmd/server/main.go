ec7be7ef3239c770283674d4903049d4c21eb0ec
Kuba Niżyński <kubanizynski@wp.pl>
2025-05-28 17:43:15 +0200

    chore: improved log

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"home.excersise/internal/server"
 )
 
 func main() {
-	log.Println("Server running at http://localhost:8080")
+	hostname, _ := os.Hostname()
+	log.Printf("Server running at http://%s:8080", hostname)
 
 	err := http.ListenAndServe(":8080", server.NewRouterWithCORS())
 	if err != nil {
